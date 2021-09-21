40049ede920ddef47e6e338263ca3d3fdbb8566f
Candice Peters <candice.peters@flatironschool.com>
2021-09-20 19:33:16 -0500

    changes

---
+++ src/comment_service.js
@@ -0,0 +1,14 @@
+class CommentService {
+    constructor(port){
+        this.port = port
+    }
+
+    getComments(){
+        fetch(this.port + `/comments`)
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch()
+    }
+
+
+}
